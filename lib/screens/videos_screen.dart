f51a116824575d94aa13c77353f5c5d0b45e8195
CodingAce99 <CodingAce99@proton.me>
2025-04-03 13:48:11 +0200

    Pantalla de videos agrupada por categorías y con miniaturas

--- lib/screens/videos_screen.dart
+++ lib/screens/videos_screen.dart
@@ -3,78 +3,131 @@ import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'VideoPlayerScreen.dart';
 
 class VideosScreen extends StatelessWidget {
-  final List<Map<String, String>> videos = [
-    {
-      'titulo': 'Bipedestación en espalderas',
-      'url': 'https://www.youtube.com/watch?v=qY24xPXy2HY',
-    },
-    {
-      'titulo': 'Colocación y cuidados del brazo pléjico',
-      'url': 'https://www.youtube.com/watch?v=Db_qJjV4HVI',
-    },
-    {
-      'titulo': 'Incentivador respiratorio',
-      'url': 'https://www.youtube.com/watch?v=oJWKUiqU1SY',
-    },
-  ];
+  final Map<String, List<Map<String, String>>> videosPorCategoria = {
+    'Fisioterapia': [
+      {
+        'titulo': 'Bipedestación en espalderas',
+        'url': 'https://www.youtube.com/watch?v=qY24xPXy2HY',
+      },
+      {
+        'titulo': 'Colocación y cuidados del brazo pléjico',
+        'url': 'https://www.youtube.com/watch?v=Db_qJjV4HVI',
+      },
+      {
+        'titulo': 'Incentivador respiratorio',
+        'url': 'https://www.youtube.com/watch?v=oJWKUiqU1SY',
+      },
+    ],
+    'Logopedia': [
+      {
+        'titulo': 'Ejercicios de Lenguaje: Automatismos',
+        'url': 'https://www.youtube.com/watch?v=w0hFSJqIsBg',
+      },
+    ],
+    'Ortopedia': [
+      {
+        'titulo': 'Colocación dafo varo ',
+        'url': 'https://www.youtube.com/watch?v=FEvXTGSb3is',
+      },
+    ],
+    'Terapia Ocupacional': [
+      {
+        'titulo': 'Apraxia alimentación',
+        'url': 'https://www.youtube.com/watch?v=zzAJCuMhft8',
+      },
+    ],
+    // Añadir mas categorias
+  };
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Videos Terapéuticos')),
-      body: ListView.builder(
-        itemCount: videos.length,
-        itemBuilder: (context, index) {
-          final video = videos[index];
-          final videoId = YoutubePlayer.convertUrlToId(video['url']!);
+      appBar: AppBar(title: Text('Videos Educativos')),
+      body: ListView(
+        children:
+            videosPorCategoria.entries.map((entry) {
+              final categoria = entry.key;
+              final videos = entry.value;
 
-          return Card(
-            margin: EdgeInsets.all(10),
-            elevation: 4,
-            child: ListTile(
-              leading: Icon(
-                Icons.play_circle_fill,
-                size: 40,
-                color: Colors.red,
-              ),
-              title: Text(video['titulo']!),
-              trailing: Icon(Icons.arrow_forward_ios),
-              onTap: () {
-                final videoId = YoutubePlayer.convertUrlToId(
-                  video['url'] ?? '',
-                );
-                if (videoId != null) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder:
-                          (_) => VideoPlayerScreen(
-                            videoId: videoId,
-                            titulo: video['titulo']!,
-                          ),
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Título de la categoría
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                    child: Text(
+                      categoria,
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('La URL del video no es válida')),
-                  );
-                }
-                /*
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder:
-                        (_) => VideoPlayerScreen(
-                          videoId: videoId!,
-                          titulo: video['titulo']!,
-                        ),
-                    
                   ),
-                );*/
-              },
-            ),
-          );
-        },
+
+                  // Lista de videos dentro de la categoría
+                  ...videos.map((video) {
+                    final videoId = YoutubePlayer.convertUrlToId(
+                      video['url'] ?? '',
+                    );
+                    if (videoId == null) return SizedBox(); // seguridad
+
+                    final thumbnailUrl =
+                        'https://img.youtube.com/vi/$videoId/0.jpg';
+
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (_) => VideoPlayerScreen(
+                                  videoId: videoId,
+                                  titulo: video['titulo']!,
+                                ),
+                          ),
+                        );
+                      },
+                      child: Card(
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 8,
+                        ),
+                        elevation: 4,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(12),
+                              ),
+                              child: Image.network(
+                                thumbnailUrl,
+                                width: double.infinity,
+                                height: 200,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.all(12),
+                              child: Text(
+                                video['titulo']!,
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ],
+              );
+            }).toList(),
       ),
     );
   }
